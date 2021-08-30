989669fa4f393a2c634ba4597ca4b4018b6c25aa
grmono <gregory-mon@protonmail.com>
2021-08-30 12:30:46 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nft-generator
+# generative-art-node
+Create generative art by using the canvas api and node js
+
+## Dependencies
+`
+npm install canvas
+`
---
+++ input/config.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const width = 1000;
+const height = 1000;
+const dir = __dirname;
+const rarity = [
+  { key: "", val: "original" },
+  { key: "_r", val: "rare" },
+  { key: "_sr", val: "super rare" },
+];
+
+const addRarity = (_str) => {
+  let itemRarity;
+  rarity.forEach((r) => {
+    if (_str.includes(r.key)) {
+      itemRarity = r.val;
+    }
+  });
+  return itemRarity;
+};
+
+const cleanName = (_str) => {
+  let name = _str.slice(0, -4);
+  rarity.forEach((r) => {
+    name = name.replace(r.key, "");
+  });
+  return name;
+};
+
+const getElements = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index + 1,
+        name: cleanName(i),
+        fileName: i,
+        rarity: addRarity(i),
+      };
+    });
+};
+
+const layers = [
+  {
+    id: 1,
+    name: "background",
+    location: `${dir}/background/`,
+    elements: getElements(`${dir}/background/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 2,
+    name: "onion",
+    location: `${dir}/onion/`,
+    elements: getElements(`${dir}/onion/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 3,
+    name: "base",
+    location: `${dir}/base/`,
+    elements: getElements(`${dir}/base/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 4,
+    name: "eyes",
+    location: `${dir}/eyes/`,
+    elements: getElements(`${dir}/eyes/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 5,
+    name: "mouth",
+    location: `${dir}/mouth/`,
+    elements: getElements(`${dir}/mouth/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 6,
+    name: "hat",
+    location: `${dir}/hat/`,
+    elements: getElements(`${dir}/hat/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  }
+];
+
+module.exports = { layers, width, height };
